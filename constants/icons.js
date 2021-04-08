1068a64a4646b245b9bf5e9c24cbee86676bc086
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-04-08 23:05:11 +0100

    created constants for App icons

---
+++ constants/icons.js
@@ -0,0 +1,23 @@
+// Nav Icons
+const location      = require("../assets/icons/material-location-on.png");
+const sheild        = require("../assets/icons/awesome-shield-alt.png");
+const settings      = require("../assets/icons/feather-settings.png");
+
+// Weather Icons 
+
+const sunny         = require("../assets/icons/weather-day-sunny.png");
+const thunder       = require("../assets/icons/weather-thunderstorm.png");
+const metro         = require("../assets/icons/metro-weather.png");
+const rainy         = require("../assets/icons/feather-cloud-rain.png");
+
+
+export {
+    location,
+    sheild,
+    settings,
+    sunny,
+    thunder,
+    metro,
+    rainy,
+}
+
